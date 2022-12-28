7db3c09720dab3111572779625971b8b3588c8af
freeklinssen <freeklinssen24@gmail.com>
2022-12-28 14:18:22 +0100

    finally

--- simple_mnist.py
+++ simple_mnist.py
@@ -1,1 +1,46 @@
 import numpy as np
+import os
+import sys
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), 'tinygrad'))
+sys.path.insert(1, '/path/to/application/app/folder')
+
+import numpy as np
+from tinygrad.tinygrad.tensor import Tensor
+from tinygrad.tinygrad.nn import BatchNorm2D
+from tinygrad.extra.utils import get_parameters
+from tinygrad.datasets import fetch_mnist
+from tinygrad.extra.training import train, evaluate, sparse_categorical_crossentropy
+import tinygrad.tinygrad.nn.optim as optim
+from tinygrad.extra.augment import augment_img
+
+
+
+class ConvBlock:
+  def __init__(self, h, w, inp, filters=32, conv=3):
+    self.h, self.w = h, w
+    self.inp = inp
+    #init weights
+    self.weights = [Tensor.uniform(filters, inp if i==0 else filters, conv, conv) for i in range(3)]
+    self.biases = [Tensor.uniform(1, filters, 1, 1) for i in range(3)]
+    #init layers
+    self._bn = BatchNorm2D(filters)
+
+  def __call__(self, input):
+    x = input.reshape(shape=(-1, self.inp, self.w, self.h))
+    for cweight, cbias in zip(self.weights, self.biases):
+      x = x.pad2d(padding=[1,1,1,1]).conv2d(cweight).add(cbias).relu()
+    
+    x = self._bn(x)
+    
+    return x
+  
+
+if __name__ == "__main__":
+  
+  tensor = Tensor.uniform(5,5)
+  print(tensor)
+  print('okay')
+  
+  lmbd = 0.00025
+  lossfn = lambda out,y: sparse_categorical_crossentropy(out, y) + lmbd*(model.weight1.abs() + model.weight2.abs()).sum()
