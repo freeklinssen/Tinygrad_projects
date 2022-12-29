08f2a28437680deb74ccf99ecd9fa361c926babc
freeklinssen <freeklinssen24@gmail.com>
2022-12-29 13:02:25 +0100

    train and evaluate loop

--- simple_mnist.py
+++ simple_mnist.py
@@ -1,18 +1,12 @@
 import numpy as np
-import os
-import sys
-sys.path.append(os.getcwd())
-
-
-import numpy as np
+from sklearn import datasets
 from tinygrad.tinygrad.tensor import Tensor
 from tinygrad.tinygrad.nn import BatchNorm2D
 from tinygrad.extra.utils import get_parameters
-from tinygrad.datasets import fetch_mnist
-from tinygrad.extra.training import train, evaluate, sparse_categorical_crossentropy
+from tinygrad.extra.training import sparse_categorical_crossentropy
 import tinygrad.tinygrad.nn.optim as optim
-from tinygrad.extra.augment import augment_img
 
+small = False
 
 
 class Convolution:
@@ -74,21 +68,75 @@ class large_model:
     return x
     
     
-def train(x):
-  return x
+def train(model, x_train, y_train, optimizer, steps, lossfn,  bs=32):
+  
+  losses, accuracies = [], []
+  Tensor.training = True
+  for step in range(steps):
+    numbers = np.random.uniform(0, len(x_train), bs)
+    x= Tensor(x_train[numbers])
+    y = Tensor(y_train[numbers])
+    
+    out = model.forward(x)
+    loss = lossfn(out, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    
+    loss = loss.data
+    out = np.argmax(out.data, axis=-1)
+    accuracy = (out == y).mean()
+    
+    print('loss: %.2f,accuracy: %.2f' % (loss, accuracy))
+    
+    losses.append(loss)
+    accuracies.append(accuracy)
+    
+  return losses, accuracies
+    
 
-def validation(x):
-  return x
-
-
+def evaluate(model, x_test, y_test, lossfn, bs=32, max_size = 120 ):
+  
+  Tensor.training = False
+  epochs = max_size//bs
+  numbers = np.random(0, len(x_test), epochs*bs)
+  x = Tensor(x_test[numbers])
+  y = y_test[numbers]
+  
+  for i in range(epochs):
+    predictions[i*bs:(i+1)*bs] = model.forward(x[i*bs:(i+1)*bs])
+    
+  predictions = np.argmax(predictions.data, axis=-1)
+  accuracy = (predictions == y).mean()
+  
+  print('test loss: %.2f, test accuracy: %.2f' % accuracy)
+  
+  return None 
      
 
 if __name__ == "__main__":
+  (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+  x_train = x_train.reshape(-1, 28, 28).astype(np.uint8)
+  x_test = x_test.reshape(-1, 28, 28).astype(np.uint8)
+  y_train = y_train.astype(np.uint8)
+  y_test = y_test.astype(np.uint8)
   
-  lrt = 
+  lrts = [1e-4, 1e-5]
   epochs = [10, 10]
-  
+  batch_size = 32
+  steps = 20
   
   model = large_model()
+  
+  if small:
+    steps = 10
+    model = small_model()
+  
   lmbd = 0.00025
-  lossfn = lambda out,y: sparse_categorical_crossentropy(out, y) + lmbd*(model.weight1.abs() + model.weight2.abs()).sum()
+  lossfn = lambda out,y: sparse_categorical_crossentropy(out, y) + lmbd*(model.linear1.abs().sum() + model.linear2.abs().sum())
+  
+  for lrt, epoch in zip(lrts, epochs):
+    optimizer = optim.Adam(model.parameters(), lr=lrt)
+    for i in range(epoch):
+      train(model, x_train, y_train, optimizer, steps=steps, lossfn=lossfn, bs=batch_size)
+      evaluate(model, x_train, y_train, max_size=120)
