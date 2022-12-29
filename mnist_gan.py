1e711e1b5b1c90a85be5df0213b865309a6a2031
freeklinssen <freeklinssen24@gmail.com>
2022-12-30 00:14:21 +0100

    starting with the train loop

--- mnist_gan.py
+++ mnist_gan.py
@@ -12,16 +12,14 @@ from extra.training import sparse_categorical_crossentropy
 import tinygrad.nn.optim as optim
 
 
-
 class generator:
   def __init__(self, w, h, debt=1):
     self.h, self.w = w, h
     self.debt = debt
-    #self.noise = np.random.normal(0, 1, 100)
-    self.linear1 = Tensor.linear(100, 256)
-    self.linear2 = Tensor.linear(256, 512)
-    self.linear3 = Tensor.linear(512, 1024)
-    self.linear4 = Tensor.linear(1024, w*h*debt)
+    self.linear1 = Tensor.uniform(100, 256)
+    self.linear2 = Tensor.uniform(256, 512)
+    self.linear3 = Tensor.uniform(512, 1024)
+    self.linear4 = Tensor.uniform(1024, w*h*debt)
     
     self.bn = BatchNorm2D(1)
 
@@ -40,9 +38,9 @@ class generator:
 class discriminator:
   def __init__(self, w, h, depth=1):
     self.size = w*h*depth
-    self.linear1 = Tensor.linear(self.size, 512)
-    self.linear2 = Tensor.linear(512, 256)
-    self.linear3 = Tensor.linear(256, 1)
+    self.linear1 = Tensor.uniform(self.size, 512)
+    self.linear2 = Tensor.uniform(512, 256)
+    self.linear3 = Tensor.uniform(256, 1)
   
   def forward(self, x):
     x = x.reshape(shape=(-1, self.size))
@@ -51,7 +49,39 @@ class discriminator:
     return x
   
 
+def train(x_train, epochs, BS = 32):
+  
+  half_BS = BS//2
+  y_real = np.ones((BS, 1))
+  y_fake = np.zeros((BS, 1))
+  
+  for epoch in range(epochs):
+    
+    ############# train discriminator:
+    #real input:
+    numbers = np.random.randint(0, len(x_train), BS)
+    x_real = Tensor(x_train[numbers])
+    
+    #fake input
+    noise = Tensor(np.random.normal(0, 1, (BS, 100)))
+    x_fake = generator.forward(noise)
+    
+    P_y_real = discriminator.forward(x_real)
+    P_y_fake = discriminator.forward(x_fake)
+    
+    # hier moet nog de loss berekent worden met de and backpropogated 
+    
+    ############ train generator:
+    
+    noise = Tensor(np.random.normal(0, 1, (BS, 100)))
 
+    
+    
+    
+    
+  
+  
+  
     
     
     
@@ -64,5 +94,11 @@ if __name__ == "__main__":
   y_train = y_train.astype(np.uint8)
   y_test = y_test.astype(np.uint8)
   
-  print(np.random.normal(0, 1, (25, 100)))
+  generator = generator(28, 28)
+  discriminator = discriminator(28, 28)
+  
+    
+  print(np.ones((12, 1)))
+  #self.noise = np.random.normal(0, 1, 100)
+  #print(np.random.normal(0, 1, (25, 100)))
     
