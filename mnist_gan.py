b55f1dfde3c5ad5b4d8bcb32a7e340bd9d5f57de
freeklinssen <freeklinssen24@gmail.com>
2022-12-30 22:18:54 +0100

    some improvements to the gan

--- mnist_gan.py
+++ mnist_gan.py
@@ -8,7 +8,6 @@ from tensorflow import keras
 from tinygrad.tensor import Tensor
 from tinygrad.nn import BatchNorm2D
 from extra.utils import get_parameters
-from extra.training import sparse_categorical_crossentropy
 import tinygrad.nn.optim as optim
 
 
@@ -22,6 +21,9 @@ class generator:
     self.linear4 = Tensor.uniform(1024, w*h*debt)
     
     self.bn = BatchNorm2D(1)
+  
+  def parameters(self):
+    return get_parameters(self)
 
   def forward(self, noise):
     x = noise.dot(self.linear1)
@@ -42,63 +44,110 @@ class discriminator:
     self.linear2 = Tensor.uniform(512, 256)
     self.linear3 = Tensor.uniform(256, 1)
   
+  def parameters(self):
+    return get_parameters(self)
+  
   def forward(self, x):
     x = x.reshape(shape=(-1, self.size))
-    x = x.dot(self.linear1).relu()
-    x = x.dot(self.linear2).sigmoid()
+    x = x.dot(self.linear1).leakyrelu(0.2)
+    x = x.dot(self.linear2).leakyrelu(0.2)
+    x = x.dot(self.linear3).sigmoid()
     return x
+ 
   
-
-def train(x_train, epochs, BS = 32):
+def binary_crossentropy(out, y):
   
-  half_BS = BS//2
-  y_real = np.ones((BS, 1))
+  return None 
+   
+  
+def train(generator, discriminator, x_train, lossfn, optimizer_g, optimizer_d, epoch, steps=100, BS = 32):
+  
+  Tensor.training = True
+  y_real = np.ones((BS, 1))    # dit kan mischien nog anders i.e. andere target waardes
   y_fake = np.zeros((BS, 1))
-  
-  for epoch in range(epochs):
     
-    ############# train discriminator:
-    #real input:
-    numbers = np.random.randint(0, len(x_train), BS)
-    x_real = Tensor(x_train[numbers])
-    
-    #fake input
-    noise = Tensor(np.random.normal(0, 1, (BS, 100)))
-    x_fake = generator.forward(noise)
-    
-    P_y_real = discriminator.forward(x_real)
-    P_y_fake = discriminator.forward(x_fake)
-    
-    # hier moet nog de loss berekent worden met de and backpropogated 
+  for step in steps:
+    for i in range(4):
+      ############# train discriminator:
+      #real input:
+      numbers = np.random.randint(0, len(x_train), BS)
+      x_real = Tensor(x_train[numbers])
+      
+      output_real = discriminator.forward(x_real)
+      loss_real = lossfn(output_real, y_real) # klopt niet
+      optimizer_d.zero_grad()
+      loss_real.backward()
+      optimizer_d.step()
+      loss_real = loss_real.data
+      output_real = np.argmax(output_real.data, axis=-1)
+      accuracy_real = (output_real == y_real).mean()
+      
+      
+      #fake input
+      noise = Tensor(np.random.normal(0, 1, (BS, 100)))
+      x_fake = generator.forward(noise).detach()
+      
+      output_fake = discriminator.forward(x_fake)
+      loss_fake = lossfn(output_fake, y_fake) # klopt niet
+      optimizer_d.zero_grad()
+      loss_fake.backward()
+      optimizer_d.step()
+      loss_fake = loss_fake.data
+      output_fake = np.argmax(output_fake.data, axis=-1)
+      accuracy_fake = (output_fake == y_real).mean()
+      
+      accuracy_discriminator = np.mean(accuracy_real, accuracy_fake)
+      loss_discriminator = loss_real + loss_fake
+      #total_predictions = np.concatenate((, accuracy_fake), axis=-1) 
+      
     
     ############ train generator:
     
     noise = Tensor(np.random.normal(0, 1, (BS, 100)))
+    x_fake = generator.forward(noise)
+    output_fake = discriminator.forward(x_fake)
+    loss_fake = lossfn(output_fake, y_real)
+    optimizer_g.zero_grad()
+    loss_fake.backward()
+    optimizer_g.step()
+    loss_generator = loss_fake.data
+    output_fake = np.argmax(output_fake.data, axis=-1)
+    accuracy_generator = (output_fake == y_real).mean()
 
-    
-    
-    
-    
+
+  #print some output of the generator
+  noise = Tensor(np.random.normal(0, 1, (25, 100)))
+  generated_images = generator.forward(noise).detach()
+  h,w = 5, 5
+  fig, axs = plt.subplots(r, c)
+  count = 0
+  for i in range(h):
+      for j in range(w):
+          axs[i,j].imshow(generated_images[count, :,:,0], cmap='gray')
+          axs[i,j].axis('off')
+          count += 1
+  fig.savefig("images/%d.png" % epoch)
   
-  
-  
+  return  
     
     
     
 
 if __name__ == "__main__":
   
-  (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-  x_train = x_train.reshape(-1, 28, 28).astype(np.uint8)
-  x_test = x_test.reshape(-1, 28, 28).astype(np.uint8)
-  y_train = y_train.astype(np.uint8)
-  y_test = y_test.astype(np.uint8)
+  (x_train,_), (_, _) = keras.datasets.mnist.load_data()
+  x_train = x_train.reshape(-1, 28, 28).astype(np.uint32)
   
   generator = generator(28, 28)
   discriminator = discriminator(28, 28)
   
-    
-  print(np.ones((12, 1)))
-  #self.noise = np.random.normal(0, 1, 100)
-  #print(np.random.normal(0, 1, (25, 100)))
-    
+  epochs = 10
+  steps = 10
+  batch_size = 32
+  loss_function = lambda out, y: binary_crossentropy(out, y)
+  
+  optim_g = optim.Adam(generator.parameters(), lr=0.0002, b1=0.5) 
+  optim_d = optim.Adam(discriminator.parameters(), lr=0.0002, b1=0.5)
+  
+  for epoch in range(epochs):
+    train(generator, discriminator, x_train, loss_function, optim_g, optim_d, epoch, BS=batch_size)
